d55abc49ca9f9653984cee4e6b8c50f0e6794e99
Davie Janeway <davie.janeway@btinternet.com>
2017-07-05 00:30:29 +0100

    Changed dep version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "OrbitFrontend",
     dependencies: [
-        .Package(url: "https://github.com/daviejaneway/OrbitCompilerUtils.git", majorVersion: 1)
+        .Package(url: "https://github.com/daviejaneway/OrbitCompilerUtils.git", majorVersion: 0)
     ]
 )
